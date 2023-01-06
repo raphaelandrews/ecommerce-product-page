7f1dc5a97917c686881fb54ac1113c0b828759ff
Raphael Andrews <raphaelandrews95@gmail.com>
2023-01-06 18:47:13 -0300

    feat: increase cart products

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,23 @@
-/*=== Product Count ===*/
-function totalClick(click) {
-    const totalClicks = document.getElementById("totalClicks");
-    const sumValue = parseInt(totalClicks.innerText) + click;
-    totalClicks.innerText = sumValue;
+const totalProducts = document.querySelector("#totalProducts");
+const product_btn = document.querySelector(".product-btn");
+const count = document.querySelector(".cart-count");
+let count_value = 0;
 
-    if(sumValue < 0) {
-        totalClicks.innerText = 0;
+product_btn.addEventListener('click', onSubmit);
+
+/*=== Product Count ===*/
+function totalProduct(click) {
+    const sumValue = parseInt(totalProducts.innerText) + click;
+    totalProducts.innerText = sumValue;
+
+    if (sumValue < 0) {
+        totalProducts.innerText = 0;
     }
 }
+
+/*=== Update Procudc Count ===*/
+function onSubmit() {
+    count.textContent = parseInt(totalProducts.innerText) + parseInt(count.textContent);
+}
+
+

b8c553797f56972d936030080a141231597e51c0
Raphael Andrews <raphaelandrews95@gmail.com>
2023-01-07 14:58:58 -0300

    feat: delete cart items

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ const cardContainer = document.querySelector(".card-container");
 const productTitle = document.querySelector(".product-title");
 const carouselPrice = document.querySelector(".carousel-price");
 const carouselPriceContent = document.querySelector(".carousel-price").textContent;
+const card = document.querySelector(".card");
 let count_value = 0;
 
 productBtn.addEventListener('click', onSubmit);
@@ -14,7 +15,7 @@ const cartState = {
     'empty': `<div class="p-5"><p class="fw-bold text-center m-0">Your cart is empty.</p></div>`,
     'full': `<div class="d-flex align-items-center justify-content-between mb-4">
                 <img src="./assets/img/image-product-1-thumbnail.jpg" class="card-img d-block" alt="...">
-                <div>
+                <div class="text-start">
                     <h5 class="card-title">${productTitle.textContent}</h5>
                     <div>
                         <span>${"$" + carouselPrice.textContent.slice(1)}</span>
@@ -23,7 +24,7 @@ const cartState = {
                         <span class="product-total fw-bold"></span>
                     </div>
                 </div>
-                <img src="./assets/img/icon-delete.svg" >
+                <img src="./assets/img/icon-delete.svg" class="card-delete" onclick="deleteProducts()">
             </div>
     <a href="#" class="card-btn btn w-100 fw-bold">Checkout</a>`
 }
@@ -42,10 +43,10 @@ function totalProduct(click) {
 function onSubmit() {
     count.textContent = parseInt(totalProducts.innerText) + parseInt(count.textContent);
     totalProducts.innerText = 0;
-    cardContainer.setAttribute('style', 'display:flex !important');
+   /* cardContainer.setAttribute('style', 'display:flex !important');*/
+   card.setAttribute('style', 'display:flex !important');
     checkCountValue();
     defineCartState(count);
-    totalValue();
 }
 
 function checkCountValue() {
@@ -57,7 +58,6 @@ function checkCountValue() {
 }
 checkCountValue();
 
-
 function defineCartState(count) {
     if (count.textContent === '0') {
         cardBody.innerHTML = cartState.empty;
@@ -68,4 +68,10 @@ function defineCartState(count) {
         product_quantity.innerHTML = count.textContent;
         product_total.innerHTML = "$" + parseInt(carouselPriceContent.slice(1)) * parseInt(product_quantity.textContent) + ".00";
     }
+}
+
+function deleteProducts() {
+    count.textContent = "0";
+    defineCartState(count);
+    checkCountValue();
 }
